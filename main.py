c8b0cf1cbb4441111064e2155fb8870f25db76f4
Aditya Verulkar <137478308+adityaverulkar@users.noreply.github.com>
2025-04-20 10:45:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,9 @@ def get_args_parser():
     # Model parameters
     parser.add_argument('--frozen_weights', type=str, default=None,
                         help="Path to the pretrained model. If set, only the mask head will be trained")
+    
+    parser.add_argument('--num_classes', default=91, type=int, help="Number of object classes")
+
     # * Backbone
     parser.add_argument('--backbone', default='resnet50', type=str,
                         help="Name of the convolutional backbone to use")
